3a2889b6672191cbaaae4852e52c406ff48bc79c
Ken <devoeke@oregonstate.edu>
2020-02-16 17:16:23 +0900

    Sunday update

--- hello-world.py
+++ hello-world.py
@@ -1,2 +1,7 @@
-print("This is the first file for github")
-print("Line 2")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num1 == 0 and num2 == 2:
+    print("You got the numbers correct!")
+else:
+    print("Not so good...")
